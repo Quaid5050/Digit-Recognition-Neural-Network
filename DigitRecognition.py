3e8694ace460e9873eb9ad2aafe3d03a60d54ce7
Quaid Ahmed <inoxent5050@gmail.com>
2023-07-12 09:58:44 +0500

    train the model

--- DigitRecognition.py
+++ DigitRecognition.py
@@ -46,16 +46,32 @@ else:
     model = tf.keras.models.load_model('handwritten_digits.model')
 
 # Load custom images and predict them
-image_number = 1
-while os.path.isfile('digits/digit{}.png'.format(image_number)):
-    try:
-        img = cv2.imread('digits/digit{}.png'.format(image_number))[:,:,0]
-        img = np.invert(np.array([img]))
-        prediction = model.predict(img)
-        print("The number is probably a {}".format(np.argmax(prediction)))
-        plt.imshow(img[0], cmap=plt.cm.binary)
-        plt.show()
-        image_number += 1
-    except:
-        print("Error reading image! Proceeding with next image...")
-        image_number += 1
+# image_number = 0
+# while os.path.isfile('digits/digit{}.png'.format(image_number)):
+#     try:
+#         img = cv2.imread('digits/digit{}.png'.format(image_number))[:,:,0]
+#         img = np.invert(np.array([img]))
+#         prediction = model.predict(img)
+#         print("The number is probably a {}".format(np.argmax(prediction)))
+#         plt.imshow(img[0], cmap=plt.cm.binary)
+#         plt.show()
+#         image_number += 1
+#     except:
+#         print("Error reading image! Proceeding with next image...")
+#         image_number += 1
+#
+
+# for single image
+# img = cv2.imread('digits/digit0.png')
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convert image to grayscale
+# img = cv2.resize(img, (28, 28))  # Resize image to (28, 28)
+# img = img.reshape(1, 784)  # Flatten image to a 1D array of length 784
+# img = img / 255.0  # Normalize pixel values between 0 and 1
+# prediction = model.predict(img)
+# predicted_number = np.argmax(prediction)
+# print("The number is probably a {}".format(predicted_number))
+# plt.imshow(img.reshape(28, 28), cmap=plt.cm.binary)
+# plt.show()
+
+
+
